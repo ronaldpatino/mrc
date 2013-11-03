eea065eaca7d6602e9faa19821f39a989da8e535
ronaldpatino <ronaldpatino@gmail.com>
2013-11-02 20:13:20 -0500

    Corregido centrado de logo

--- blocks/logo.php
+++ blocks/logo.php
@@ -2,7 +2,7 @@
         <div class="span12 logo">
             <ul class="thumbnails  publicidad-top">
                 <li class="span12">
-                    <div class="thumbnail">
+                    <div class="thumbnail thumbail-center">
                         <a href="<?php echo get_home_url()?>"><img src="<?php bloginfo('template_url'); ?>/assets/img/logo.png" alt="<?php  bloginfo('name');?>" ></a>
                     </div>
                 </li>
