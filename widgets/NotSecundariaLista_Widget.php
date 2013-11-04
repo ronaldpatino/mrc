f83935dd07d5581374d8435066aee1a5d78c92b3
ronaldpatino <ronaldpatino@gmail.com>
2013-11-04 13:36:24 -0500

    Filtrando categorias no desadas en post

--- widgets/NotSecundariaLista_Widget.php
+++ widgets/NotSecundariaLista_Widget.php
@@ -17,7 +17,23 @@ class NotSecundariaLista extends WP_Widget {
 
         if ($instance['categoria'] == -1) {return;}
 
-        $NOTICIA_SECUNDARIA_LISTA = $instance['categoria'];
+        $MAX_ITEMS_LISTA = 5;
+
+        $NOTICIA_SECUNDARIA_LISTA = get_id_real($instance['categoria']);
+        $NOTICIA_PORTADA = get_id_real(54);
+        $NOTICIA_DESTACADA_SECCION_CAT_ID = 53;
+        $NOTICIA_DEPORTIVO_CUENCA_CAT_ID = 26;
+
+
+        if ($NOTICIA_SECUNDARIA_LISTA == 5 && $NOTICIA_SECUNDARIA_LISTA != 26){
+            $CATEGORIAS_EXCLUIR[0]=$NOTICIA_DESTACADA_SECCION_CAT_ID;
+            $CATEGORIAS_EXCLUIR[1]=$NOTICIA_DEPORTIVO_CUENCA_CAT_ID;
+        }
+        else
+        {
+            $CATEGORIAS_EXCLUIR[]=$NOTICIA_DESTACADA_SECCION_CAT_ID;
+        }
+
 
         global $wpdb, $post;
 
@@ -27,9 +43,9 @@ class NotSecundariaLista extends WP_Widget {
                 WHERE  ( (
                                     SELECT COUNT(1)
                                     FROM wp_04vcw8_term_relationships
-                                    WHERE term_taxonomy_id IN ($NOTICIA_SECUNDARIA_LISTA, 4249)
+                                    WHERE term_taxonomy_id IN ($NOTICIA_SECUNDARIA_LISTA, $NOTICIA_PORTADA)
                                     AND object_id = wp_04vcw8_posts.ID
-                                ) = 1 )
+                                ) = 2 )
                 AND
                     wp_04vcw8_posts.post_type = 'post'
                 AND (wp_04vcw8_posts.post_status = 'publish')
@@ -38,30 +54,51 @@ class NotSecundariaLista extends WP_Widget {
                 ORDER BY
                     wp_04vcw8_posts.post_date
                 DESC
-                LIMIT 0, 5";
+                LIMIT 0, 20";
 
 
         $noticiasSecundariaLista = $wpdb->get_results($sql);
 
         $noticia_secundaria_lista_seccion = '<div class="span4 noticia-tricol">';
 
+        $count = 0;
         foreach ($noticiasSecundariaLista as $post)
         {
-            setup_postdata($post);
-            $permalink = get_permalink();
+            $category = get_the_category($post->ID);
 
-            $noticia_secundaria_lista_seccion .= '<div class="media ml2p">';
-            $noticia_secundaria_lista_seccion .= '<a class="pull-left" href="' . get_permalink() .'">';
+            $mostrar = true;
 
-            $imagen = get_featured_image($post->ID);
-            $src= getphpthumburl($imagen, 'w=120&h=74&zc=1&q=90');
+            foreach($category as $c)
+            {
+                if (in_array($c->cat_ID, $CATEGORIAS_EXCLUIR)){
+                    $mostrar = false;
+                    break;
+                }
+            }
+
+            if ($mostrar)
+            {
+                setup_postdata($post);
+                $permalink = get_permalink();
+
+                $noticia_secundaria_lista_seccion .= '<div class="media ml2p">';
+                $noticia_secundaria_lista_seccion .= '<a class="pull-left" href="' . get_permalink() .'">';
+
+                $imagen = get_featured_image($post->ID);
+                $src= getphpthumburl($imagen, 'w=120&h=74&zc=1&q=90');
 
 
-            $noticia_secundaria_lista_seccion .= '<img src="' . $src . '" alt="' . $post->post_title . '  - El Mercurio de Cuenca Noticias Tiempo  Ecuador Azuay" title="' . $post->post_title . '  - El Mercurio de Cuenca Noticias Tiempo  Ecuador Azuay">';
-            $noticia_secundaria_lista_seccion .= '</a>';
-            $noticia_secundaria_lista_seccion .= '<div class="media-body media-body-tricol">' .'<a href="' . $permalink .'">' . $post->post_title . '</a></div>';
-            $noticia_secundaria_lista_seccion .= '</div>';
+                $noticia_secundaria_lista_seccion .= '<img src="' . $src . '" alt="' . $post->post_title . '  - El Mercurio de Cuenca Noticias Tiempo  Ecuador Azuay" title="' . $post->post_title . '  - El Mercurio de Cuenca Noticias Tiempo  Ecuador Azuay">';
+                $noticia_secundaria_lista_seccion .= '</a>';
+                $noticia_secundaria_lista_seccion .= '<div class="media-body media-body-tricol">' .'<a href="' . $permalink .'">' . $post->post_title . '</a></div>';
+                $noticia_secundaria_lista_seccion .= '</div>';
 
+                $count++;
+            }
+
+            if ($count == $MAX_ITEMS_LISTA){
+                break;
+            }
         }
 
         $noticia_secundaria_lista_seccion .= '</div>';
@@ -88,5 +125,3 @@ class NotSecundariaLista extends WP_Widget {
 }
 
 register_widget('NotSecundariaLista');
-
-?>
