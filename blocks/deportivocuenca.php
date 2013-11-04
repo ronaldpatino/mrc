0e11ea10f78e78d9a3d45c66e9fef19d3a06eaf8
ronaldpatino <ronaldpatino@gmail.com>
2013-11-04 13:36:49 -0500

    Activado contenido DC

--- blocks/deportivocuenca.php
+++ blocks/deportivocuenca.php
@@ -1,13 +1,13 @@
 <!-- Deportivo Cuenca -->
-<!--
+
 <div class='container margen-container'>
     <div id='deportivocuenca' class='row-fluid'>
-        <h2><a href="<?php //echo get_home_url()?>/deportivo-cuenca" >Deportivo Cuenca</a></h2>
+        <h2><a href="<?php echo get_home_url()?>/deportivo-cuenca" >Deportivo Cuenca</a></h2>
         <div class="row-fluid">
-            <?php //if ( dynamic_sidebar('deportivocuenca') ) : else : endif; ?>
-            <?php //get_template_part( 'blocks/publicidaddeportivocuenca' ); ?>
+            <?php if ( dynamic_sidebar('deportivocuenca') ) : else : endif; ?>
+            <?php get_template_part( 'blocks/publicidaddeportivocuenca' ); ?>
         </div>
     </div>
 </div>
--->
+
 <!-- Fin Deportivo Cuenca -->
