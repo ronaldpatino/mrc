f3865e21ef112ea74989cfd37fc846a7be7675cd
ronaldpatino <ronaldpatino@gmail.com>
2013-11-04 19:42:22 -0500

    CAmbiado sentido de la fecha del select

--- widgets/NotSecundariaLista_Widget.php
+++ widgets/NotSecundariaLista_Widget.php
@@ -53,7 +53,7 @@ class NotSecundariaLista extends WP_Widget {
                     wp_04vcw8_posts.ID
                 ORDER BY
                     wp_04vcw8_posts.post_date
-                DESC
+                ASC
                 LIMIT 0, 20";
 
 
