b5a9eeb5fca100367383bc069037c2357e04e879
ronaldpatino <ronaldpatino@gmail.com>
2013-11-04 19:32:22 -0500

    escondo banner top cuando RS

--- blocks/publicidadcabecera.php
+++ blocks/publicidadcabecera.php
@@ -1,5 +1,5 @@
 <!-- Publicidad -->
-<div class='container'>
+<div class='container hidden-phone'>
     <div class='row-fluid'>
         <?php if ( dynamic_sidebar('publicidadcabecera') ) : else : endif; ?>
     </div>
