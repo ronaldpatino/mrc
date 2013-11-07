dfcd7ff7818faec4ef0f02acd34397a761c74a34
ronaldpatino <ronaldpatino@gmail.com>
2013-11-06 20:35:41 -0500

    Fix para img single

--- single.php
+++ single.php
@@ -63,8 +63,7 @@
                 if (has_post_thumbnail()) {
 
                     $imagen = get_featured_image(get_the_ID());
-                    $src= 'thumbs/332x260/' . $imagen;
-
+                    $src= '/thumbs/332x260/' . $imagen;
                     echo '<img  src="' . $src . '" ' . 'alt="' . get_the_title() . ' - El Mercurio de Cuenca Noticias Tiempo  Ecuador Azuay" title="' . get_the_title() . '  - El Mercurio de Cuenca Noticias Tiempo  Ecuador Azuay" >';
 
                 }
