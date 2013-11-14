353579a20ffeb0c5a1ce9e9288156ffc1c668756
Ronald Patino <ronaldpatino@gmail.com>
2013-11-13 19:40:54 -0500

    Update PortadaImpresa_Widget.php

--- widgets/PortadaImpresa_Widget.php
+++ widgets/PortadaImpresa_Widget.php
@@ -18,54 +18,24 @@ class PortadaImpresa extends WP_Widget {
 
         $NOTICIA_PORTADA_IMPRESA = 8043;
 
-        global $wpdb, $post;
-
-        $sql = "    SELECT
-					a.ID, a.post_date, a.post_title, a.post_content, a.post_author, a.post_name, a.post_excerpt, a.comment_status
-
-                FROM wp_04vcw8_posts a, wp_04vcw8_term_relationships b, wp_04vcw8_term_taxonomy c
-
-				WHERE
-
-					a.id = b.object_id
-
-				AND
-					b.term_taxonomy_id = c.term_taxonomy_id
-
-				AND
-					c.term_id = '{$NOTICIA_PORTADA_IMPRESA}'
-
-				AND
-					a.post_type = 'post'
-
-				AND
-					a.post_status = 'publish'
-
-				ORDER BY
-					a.post_date
-                DESC
-
-                LIMIT 0,1";
-
-        $portadaImpresa = $wpdb->get_results($sql);
-
-        $impreso = '';
-        foreach ($portadaImpresa as $post)
-        {
-            setup_postdata($post);
-            $permalink = get_permalink();
-
-            $impreso = '<ul class="thumbnails" style="margin-top: 20px;">';
-            $impreso .= '<li class="span12 thumbnail portada" style="text-align: center;"><h3>Portada</h3>';
-
-            $imagen = get_featured_image($post->ID);
-            $src= 'thumbs/305x475/' . $imagen;
-
-            $impreso .= '<a href="' .get_home_url(). '/impresa"><img src="' . $src . '" alt="' . $post->post_title . '  - El Mercurio de Cuenca Noticias Tiempo  Ecuador Azuay" title="' . $post->post_title . '  - El Mercurio de Cuenca Noticias Tiempo  Ecuador Azuay"></a>';
-            $impreso .= '</li></ul>';
-
+        
+        $fecha =  date('Y-m-d');
+        
+        $src = get_portada_by_date($fecha);
+        
+        if (!$src){
+        	$fecha =  date('Y-m-d', strtotime('-1 day', strtotime($date_raw)));
+   	        $src = get_portada_by_date($fecha);        	
         }
-
+        
+	        $impreso = '<ul class="thumbnails" style="margin-top: 20px;">';
+	        $impreso .= '<li class="span12 thumbnail portada" style="text-align: center;"><h3>Portada</h3>';
+	
+	        $impreso .= '<a href="' .get_home_url(). '/impresa"><img src="' . $src . '" alt="Portada del ' . $fecha. '  - El Mercurio de Cuenca Noticias Tiempo  Ecuador Azuay" title="Portada del ' . $fecha . '  - El Mercurio de Cuenca Noticias Tiempo  Ecuador Azuay"></a>';
+	        $impreso .= '</li></ul>';
+	        
+        
+        
         echo $impreso;
 
     }
